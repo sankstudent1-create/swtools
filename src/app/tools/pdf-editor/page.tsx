22f29258a2c2ff8e8fed0b78583c9f598846d582
Sanket <sanketwanveinfosystems@gmail.com>
2026-04-14 14:27:28 +0530

    fix(pdf-editor): remove esm path from react-pdf css imports

--- src/app/tools/pdf-editor/page.tsx
+++ src/app/tools/pdf-editor/page.tsx
@@ -6,8 +6,8 @@ import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 import { Rnd } from 'react-rnd';
 
 // Essential CSS for react-pdf
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
